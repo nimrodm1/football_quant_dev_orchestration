234e14987fa975a726eee062b937c5c324f97cd0
Nimrod Maniv <nimrodm87@gmail.com>
2026-01-31 20:09:29 +0200

    Working workflow for backtesting develepment. Fix to test runner node.

--- 3_development/prompts/__init__.py
+++ 3_development/prompts/__init__.py
@@ -3,11 +3,13 @@ from .data_prompts import DATA_PROMPTS
 from .features_prompts import FEATURE_PROMPTS
 from .modelling_prompts import MODELLING_PROMPTS
 from .strategy_prompts import STRATEGY_PROMPTS
+from .backtest_prompts import BACKTEST_PROMPTS
 
 # 2. Map them to a name
 SPRINT_PROMPTS = {
     "data": DATA_PROMPTS,
     "features": FEATURE_PROMPTS,
     "modelling": MODELLING_PROMPTS,
-    "strategy": STRATEGY_PROMPTS
+    "strategy": STRATEGY_PROMPTS,
+    "backtest": BACKTEST_PROMPTS
 }
